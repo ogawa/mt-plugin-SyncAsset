67d53f8b1525a4fee49bba7f4ce721e2d4f4ce51
Hirotaka Ogawa <hirotaka.ogawa@gmail.com>
2010-06-21 14:50:23 +0900

    now properly remove orphaned assets and then sync newly added files to MT::Assets.

--- plugins/SyncAsset/lib/SyncAsset.pm
+++ plugins/SyncAsset/lib/SyncAsset.pm
@@ -23,42 +23,115 @@ sub create_asset {
     my $asset_pkg = MT::Asset->handler_for_file($file_name);
 
     # calculate ($w, $h, $id) and $is_image
-    my ($w, $h, $id);
-    if ($asset_pkg->isa('MT::Asset::Image')) {
-	eval { require Image::Size; };
-	die MT->translate("Perl module Image::Size is required to determine width and height of uploaded images.") if $@;
-	($w, $h, $id) = Image::Size::imgsize($file_path);
+    my ( $w, $h, $id );
+    if ( $asset_pkg->isa('MT::Asset::Image') ) {
+        eval { require Image::Size; };
+        die MT->translate(
+"Perl module Image::Size is required to determine width and height of uploaded images."
+        ) if $@;
+        ( $w, $h, $id ) = Image::Size::imgsize($file_path);
     }
     my $is_image = defined($w) && defined($h);
 
     # shorten file_path and url
-    my $site_url = $blog->site_url;
+    my $site_url  = $blog->site_url;
     my $site_path = $blog->site_path;
     $file_path =~ s!$site_path!%r!;
-    $url =~ s!$site_url!%r/!;
+    $url       =~ s!$site_url!%r/!;
 
-    my $asset = $asset_pkg->load({
-	file_path => $file_path,
-	blog_id => $blog_id,
-    });
-    if (!$asset) {
-	$asset = $asset_pkg->new();
-	$asset->file_path($file_path);
-	$asset->file_name($file_name);
-	$asset->file_ext($file_ext);
-	$asset->blog_id($blog_id);
-	$asset->created_by($user_id);
-    } else {
-	$asset->modified_by($user_id);
+    my $asset = $asset_pkg->load(
+        {
+            file_path => $file_path,
+            blog_id   => $blog_id,
+        }
+    );
+    if ( !$asset ) {
+        $asset = $asset_pkg->new();
+        $asset->file_path($file_path);
+        $asset->file_name($file_name);
+        $asset->file_ext($file_ext);
+        $asset->blog_id($blog_id);
+        $asset->created_by($user_id);
+    }
+    else {
+
+        # just for easiness
+        # we must not skip if timestamp has changed
+        return 0;
+
+        #	$asset->modified_by($user_id);
     }
     $asset->url($url);
     $asset->label($label);
     if ($is_image) {
-	$asset->image_width($w);
-	$asset->image_height($h);
+        $asset->image_width($w);
+        $asset->image_height($h);
     }
+
     #$asset->mime_type($mimetype) if $mimetype;
     $asset->save or die $asset->errstr;
+    1;
+}
+
+sub remove_asset {
+    my ($param) = @_;
+
+    my $blog_id   = $param->{blog_id};
+    my $file_path = $param->{file_path};
+    my $file_name = $param->{file_name};
+
+    require MT::Blog;
+    my $blog = MT::Blog->load($blog_id);
+
+    require MT::Asset;
+    my $asset_pkg = MT::Asset->handler_for_file($file_name);
+
+    # shorten file_path
+    my $site_path = $blog->site_path;
+    $file_path =~ s!$site_path!%r!;
+
+    my $asset = $asset_pkg->load(
+        {
+            file_path => $file_path,
+            blog_id   => $blog_id,
+        }
+    );
+    if ($asset) {
+        $asset->remove or die $asset->errstr;
+    }
+    1;
+}
+
+sub remove_orphaned_assets {
+    my ($param) = @_;
+    my $blog_id = $param->{blog_id};
+
+    require MT::Asset::Image;
+    my $iter = MT::Asset::Image->load_iter( { blog_id => $blog_id } );
+    while ( my $asset = $iter->() ) {
+        unless ( -e $asset->file_path ) {
+            $asset->remove or die $asset->errstr;
+            print STDERR "File '" . $asset->file_path . "' removed.\n";
+        }
+    }
+
+    require MT::Asset::Audio;
+    my $iter = MT::Asset::Audio->load_iter( { blog_id => $blog_id } );
+    while ( my $asset = $iter->() ) {
+        unless ( -e $asset->file_path ) {
+            $asset->remove or die $asset->errstr;
+            print STDERR "File '" . $asset->file_path . "' removed.\n";
+        }
+    }
+    require MT::Asset::Video;
+    my $iter = MT::Asset::Video->load_iter( { blog_id => $blog_id } );
+    while ( my $asset = $iter->() ) {
+        unless ( -e $asset->file_path ) {
+            $asset->remove or die $asset->errstr;
+            print STDERR "File '" . $asset->file_path . "' removed.\n";
+        }
+    }
+
     1;
 }
 
--- plugins/SyncAsset/sync_asset.pl
+++ plugins/SyncAsset/sync_asset.pl
@@ -5,7 +5,7 @@
 # This software is provided as-is. You may use it for commercial or 
 # personal use. If you distribute it, please keep this notice intact.
 #
-# Copyright (c) 2008 Hirotaka Ogawa
+# Copyright (c) 2010 Hirotaka Ogawa
 
 
 package MT::Plugin::SyncAsset;
@@ -14,16 +14,16 @@ use base qw(MT::Plugin);
 
 use MT 4.0;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 my $plugin = __PACKAGE__->new({
     id => 'sync_asset',
     name        => 'SyncAsset',
     version     => $VERSION,
     description => '<__trans phrase="SyncAsset allows you to synchronize all assets in your directory with MT asset database.">',
-    doc_link    => 'http://code.as-is.net/public/wiki/SyncAsset',
+    doc_link    => 'http://wiki.github.com/ogawa/mt-plugin-SyncAsset/',
     author_name => 'Hirotaka Ogawa',
-    author_link => 'http://as-is.net/blog/',
+    author_link => 'http://blog.as-is.net/',
     version     => $VERSION,
     l10n_class  => 'SyncAsset::L10N',
 });
